d47a62f98f9dcd5aad418cc893c8c8f7fe7d27ae
TimWong <timwong.tw93@gmail.com>
2024-05-02 14:02:29 +1000

    now generates a simple readme

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const inquirer = require(`inquirer`);
+const fs = require(`fs`);
 
 inquirer
   .prompt([
@@ -33,6 +34,12 @@ inquirer
       name: `test`,
     },
     {
+      type: `checkbox`,
+      message: `Select the license/s for your project`,
+      name: `licenses`,
+      choices: [`Apache License 2.0`, `GNU GeneralPublic License v3.0`, `MIT License`, `BSD 2-Clause Simplified License`, `BSD 3-Clause New or Revised License`, `Boost Software License 1.0`, `None`],
+    },
+    {
       type: `input`,
       message: `Enter your GitHub username`,
       name: `github`,
@@ -44,12 +51,52 @@ inquirer
     },
   ])
   .then((response) => {
-    console.log(`Your project is named: ${response.name}`);
+    console.log(`Your project is named: ${response.projectname}`);
     console.log(`Here is your project description: ${response.description}`);
     console.log(`Here are the installation instructions for your project: ${response.installation}`);
     console.log(`Here is the usuage information for your project: ${response.usage}`);
     console.log(`Here are the contribution guidelines for your project: ${response.contribution}`);
     console.log(`Here are the test instructions for your project: ${response.test}`);
+    console.log(`Here are your license/s for your project: ${response.licenses}`);
     console.log(`Here is your GitHub username: ${response.github}`);
     console.log(`Here is your email: ${response.email}`);
+
+    const readmeTemplate = `
+      # ${response.projectname}
+
+      ## Description
+      ${response.description}
+
+      ## Table of Contents
+      - [Installation](#installation)
+      - [Usage](#usuage)
+      - [Contribuitn Guidelines](#contribution-guidelines)
+      - [Test Instructions](#test-instructions)
+      - [Licenses](#licenses)
+      - [Questions]
+
+      ## Installation
+      ${response.installation}
+
+      ## Usage
+      ${response.usage}
+
+      ## Contribution Guidelines
+      ${response.contribution}
+
+      ## Test Instructions
+      ${response.test}
+
+      ## Licenses
+      ${response.licenses}
+
+      ## Questions
+      ${response.github}
+      ${response.email}
+    `;
+
+    fs.writeFile('generated-readme.md', readmeTemplate, (err) =>
+      err ? console.error(err) : console.log('Success!')
+    );
+    
   });
